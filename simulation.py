a0d4f093567b5871373d10b08ecc1d02f95e4453
Ariyan0047 <aariyan0047@gmail.com>
2020-11-30 20:51:01 +0600

    FIRST COMMIT

---
+++ simulation.py
@@ -0,0 +1,61 @@
+# IMPORTING MODULES
+import os
+import sys
+import time
+import traci
+import random
+import numpy as np
+import traci.constants
+import matplotlib.pyplot as plt
+from algorithm import ga
+
+
+# SETTING SUMO ENVIRONMENT
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("Please declare the environment variable 'SUMO_HOME'")
+
+sumoBinary = "C:\\Program Files (x86)\\Eclipse\\Sumo\\bin\\sumo-gui"
+sumoCmd = [sumoBinary, "-c", "DATASETS/Dhanmondi.sumocfg"]
+
+
+def main(value1, value2, value3):
+    traci.start(sumoCmd)
+    TLIDS = traci.trafficlight.getIDList()
+    step = 0
+    while step < 100:
+        traci.simulationStep()
+        for traffic_light_id in TLIDS:
+            if value1 < value2 and value1 < value3:
+                traci.trafficlight.setProgram(traffic_light_id, "program_01")
+            if value2 < value1 and value2 < value3:
+                traci.trafficlight.setProgram(traffic_light_id, "program_02")
+            if value3 < value1 and value3 < value2:
+                traci.trafficlight.setProgram(traffic_light_id, "program_03")
+        step += 1
+    traci.close()
+
+
+for _ in range(0, 4):
+
+    # RUNNING THE ALGORITHM
+    output_result_01 = ga.run_ga(ga.problem, ga.params)
+    output_result_02 = ga.run_ga(ga.problem, ga.params)
+    output_result_03 = ga.run_ga(ga.problem, ga.params)
+
+    # plt.semilogy(output_result_01.best_cost)
+    # plt.xlim(0, ga.params.maxit)
+    # plt.xlabel("Iterations")
+    # plt.ylabel("Best Cost")
+    # plt.title("GENETIC ALGORITHM")
+    # plt.grid(True)
+    # plt.show()
+
+    # EXTRACTING MINIMUN VALUE
+    traffic_light_01 = min(output_result_01.best_cost)
+    traffic_light_02 = min(output_result_02.best_cost)
+    traffic_light_03 = min(output_result_03.best_cost)
+
+    main(traffic_light_01, traffic_light_02, traffic_light_03)
